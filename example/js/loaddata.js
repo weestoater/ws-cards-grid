82e847d32f5ea5f69df833094066860785ca00a4
Ian Burrett <ian@weestoater.com>
2022-03-20 22:47:42 +0000

    added nationality

--- example/js/loaddata.js
+++ example/js/loaddata.js
@@ -14,20 +14,21 @@ function renderData(data) {
     data.map(function(item, index) {
         let card = '<div class="card">';
         card += '<div class="card-header">';
-        card += '<span class="id">' + (index + 1) + '</span>' + item.name.title + ' ' + item.name.first + ' ' + item.name.last + ' &middot; <small>(' + item.dob.age + ')</small></div>';
+        card += '<span class="id">' + (index + 1) + '</span>' + item.name.title + ' ' + item.name.first + ' ' + item.name.last + ' &middot; <small>(' + item.dob.age + ')</small> </div>';
         card += '<div class="card-body">';
         card += '<img src="' + item.picture.medium + '" alt="" aria-hidden="true" />';
         card += '<address>' + item.location.street.number + '  '  + item.location.street.name + ',<br />';
         card += item.location.city + ',<br />'  + item.location.state + ',<br />' + item.location.country + '.<br />';
         card += item.location.postcode + '</address><hr />';
-        card += '<p><b>Cell:</b> '+ item.cell + '<br />';
+        card += '<b>Cell:</b> '+ item.cell + '<br />';
         card += '<b>Phone:</b> '+ item.phone + '<br />';
-        card += '<b>Email:</b> '+ item.email + '</p>';
+        card += '<b>Email:</b> '+ item.email + '<br />';
+        card += '<b>Nationality:</b> '+ item.nat + '<br />';
         card += '</div>';        
         card += '</div>';
 
         cards += card;
-    }, 80);
+    }, 0);
     cards += '</div>';
     document.getElementById('cards').innerHTML = cards;
 }
