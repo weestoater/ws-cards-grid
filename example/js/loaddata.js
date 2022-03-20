d8875210ad3f91b02a35cbee311469281c870a9b
Ian Burrett <ian@weestoater.com>
2022-03-20 01:17:41 +0000

    more layout & data additions

--- example/js/loaddata.js
+++ example/js/loaddata.js
@@ -15,9 +15,12 @@ function renderData(data) {
     data.map(function(item, index) {
         let card = '<div class="card">';
         card += '<div class="card-header">';
-        card += '<b>' + (index + 1) + '</b>' + item.name.title + ' ' + item.name.first + ' ' + item.name.last + '</div>';
+        card += '<span class="id">' + (index + 1) + '</span>' + item.name.title + ' ' + item.name.first + ' ' + item.name.last + '</div>';
         card += '<div class="card-body">';
-        card += '<img src="' + item.picture.large + '" alt="" aria-hidden="true" />';
+        card += '<img src="' + item.picture.medium + '" alt="" aria-hidden="true" />';
+        card += '<address>' + item.location.street.number + '  '  + item.location.street.name + ',<br />';
+        card += item.location.city + ',<br />'  + item.location.state + ',<br />' + item.location.country + '.<br />';
+        card += item.location.postcode + '</address>';
         card += '</div>';        
         card += '</div>';
 
